f87f494aae17d954b0da4ec317b194e9c5af0aa2
luffy977338 <timur.djeletyan.2009@gmail.com>
2024-09-15 19:08:12 +0300

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  output: "export",
+};
 
 export default nextConfig;
